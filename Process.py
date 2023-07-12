ef301a9068ab2af3a0710fd2f2d465f78a50e59e
SemperSolus0x3d <75125391+SemperSolus0x3d@users.noreply.github.com>
2023-07-12 23:18:01 +0300

    Fix memory access violation

--- Process.py
+++ Process.py
@@ -1,32 +1,59 @@
 import ctypes as c
+import functools
 from WinJobsterLoader import WinJobsterLoader
+
+
+def _cleanup_on_fail(func):
+
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            return func(self, *args, **kwargs)
+        except:
+            self._cleanup()
+            raise
+
+    return wrapper
+
 
 class Process:
     _library = WinJobsterLoader().load()
 
     def __init__(self):
-        self._handle = c.c_void_p(None)
+        self._handle = None
 
+    @_cleanup_on_fail
     def start(self, cmdline: str, working_directory: str | None = None):
-        self._library.StartProcess(
-            cmdline,
-            working_directory,
-            c.byref(self._handle))
+        try:
+            self._handle = c.c_void_p(None)
+
+            self._library.StartProcess(
+                cmdline,
+                working_directory,
+                c.byref(self._handle))
+        except:
+            self._cleanup()
+            raise
 
     @property
+    @_cleanup_on_fail
     def is_alive(self) -> bool:
         if self._handle is None:
             return False
 
         return self._library.IsAlive(self._handle)
 
+    @_cleanup_on_fail
     def kill(self):
         if self._handle is None:
             return
 
         self._library.Kill(self._handle)
 
-    def __del__(self):
+    def _cleanup(self):
         if self._handle is not None:
             self._library.Cleanup(self._handle)
             self._handle = None
+
+    def __del__(self):
+        self._cleanup()
