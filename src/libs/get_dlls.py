c18bc0660a3326f4c52fd167d0f61493e3aee388
MaxBQb <cmdazaza@gmail.com>
2023-07-16 00:15:18 +0300

    Move out dlls from git, now they only downloaded when needed
    On build, on test and by developers

---
+++ src/libs/get_dlls.py
@@ -0,0 +1,23 @@
+import logging
+import os
+import re
+
+from github_release_downloader import GitHubRepo, check_and_download_updates
+from semantic_version import SimpleSpec
+
+
+def main():
+    logging.basicConfig(
+        format="[%(asctime)s][%(levelname)s] %(message)s",
+        datefmt='%H:%M:%S',
+        level=logging.INFO
+    )
+    check_and_download_updates(
+        GitHubRepo("SemperSolus0x3d", "WinJobster.cpp", os.environ.get("GITHUB_TOKEN")),
+        SimpleSpec("~1"),
+        assets_mask=re.compile(".*\\.dll"),
+    )
+
+
+if __name__ == '__main__':
+    main()
